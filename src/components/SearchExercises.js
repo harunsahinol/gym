cbd41946260abc1c192872b5d5e3a448c73d5971
Harun Sahinol <mehmethsh@gmail.com>
2024-07-01 17:49:35 +0300

    main parts are done

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,18 +1,18 @@
-import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
-import { exercisesOptions, fetchData } from "../utils/fetchData";
-import HorizontalScrollBar from "./HorizontalScrollBar";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 
-const SearchExercises = () => {
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+import HorizontalScrollbar from "./HorizontalScrollbar";
+
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
   const [search, setSearch] = useState("");
-  const [exercises, setExercises] = useState([]);
   const [bodyParts, setBodyParts] = useState([]);
 
   useEffect(() => {
     const fetchExercisesData = async () => {
       const bodyPartsData = await fetchData(
         "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
-        exercisesOptions
+        exerciseOptions
       );
 
       setBodyParts(["all", ...bodyPartsData]);
@@ -25,17 +25,21 @@ const SearchExercises = () => {
     if (search) {
       const exercisesData = await fetchData(
         "https://exercisedb.p.rapidapi.com/exercises",
-        exercisesOptions
+        exerciseOptions
       );
-      const SearchExercises = exercisesData.filter(
-        (exercise) =>
-          exercise.name.toLowerCase().includes(search) ||
-          exercise.target.toLowerCase().includes(search) ||
-          exercise.equipment.toLowerCase().includes(search) ||
-          exercise.bodyPart.toLowerCase().includes(search)
+
+      const searchedExercises = exercisesData.filter(
+        (item) =>
+          item.name.toLowerCase().includes(search) ||
+          item.target.toLowerCase().includes(search) ||
+          item.equipment.toLowerCase().includes(search) ||
+          item.bodyPart.toLowerCase().includes(search)
       );
+
+      window.scrollTo({ top: 1800, left: 100, behavior: "smooth" });
+
       setSearch("");
-      setExercises(SearchExercises);
+      setExercises(searchedExercises);
     }
   };
 
@@ -44,24 +48,20 @@ const SearchExercises = () => {
       <Typography
         fontWeight={700}
         sx={{ fontSize: { lg: "44px", xs: "30px" } }}
-        mb="50ox"
+        mb="49px"
         textAlign="center"
       >
-        Awesome Exercises You <br />
-        Should Know
+        Awesome Exercises You <br /> Should Know
       </Typography>
       <Box position="relative" mb="72px">
         <TextField
+          height="76px"
           sx={{
             input: { fontWeight: "700", border: "none", borderRadius: "4px" },
-            width: {
-              lg: "800px",
-              xs: "350px",
-              backgroundColor: "#fff",
-              border: "40px",
-            },
+            width: { lg: "1170px", xs: "350px" },
+            backgroundColor: "#fff",
+            borderRadius: "40px",
           }}
-          height="76px"
           value={search}
           onChange={(e) => setSearch(e.target.value.toLowerCase())}
           placeholder="Search Exercises"
@@ -71,22 +71,26 @@ const SearchExercises = () => {
           className="search-btn"
           sx={{
             bgcolor: "#FF2625",
-            color: "#FFF",
+            color: "#fff",
             textTransform: "none",
-            width: { lg: "175px", xs: "80px" },
-            fontSize: { lg: "20px", xs: "14px" },
+            width: { lg: "173px", xs: "80px" },
             height: "56px",
             position: "absolute",
-            right: "0",
+            right: "0px",
+            fontSize: { lg: "20px", xs: "14px" },
           }}
           onClick={handleSearch}
         >
           Search
         </Button>
       </Box>
-
-      <Box sx={{ position: "relavite", width: "%100", p: "20px" }}>
-        <HorizontalScrollBar data={bodyParts} />
+      <Box sx={{ position: "relative", width: "100%", p: "20px" }}>
+        <HorizontalScrollbar
+          data={bodyParts}
+          bodyParts
+          setBodyPart={setBodyPart}
+          bodyPart={bodyPart}
+        />
       </Box>
     </Stack>
   );
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
-
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
-const root = createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
